cd75c521a50e4a79d5593d8fa5ab98fe8d61135c
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-04-16 21:41:22 +0300

    refactor

--- report_portal/src/test/java/task/reportPortal/newWidgetTaskTest.java
+++ report_portal/src/test/java/task/reportPortal/newWidgetTaskTest.java
@@ -5,20 +5,26 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v131.security.Security;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
 
 public class newWidgetTaskTest {
     @Test
     public void NewWidgetTaskProgressTest() {
 
         WebDriver driver = new ChromeDriver();
+
         driver.get("https://demo.reportportal.io/");
 
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-
-        WebElement okButton = driver.findElement(By.xpath("//button[text()='OK']"));
-        JavascriptExecutor executor = (JavascriptExecutor) driver;
-        executor.executeScript("arguments[0].click();", okButton);
 
         driver.findElement(By.xpath("//span[contains@class, 'sidebarButton__wrapper')]")).click();
         driver.findElement(By.xpath("//div[contains" +
