d4bafe536dcd1d669022ff27a30c94c82b04bf74
Alexandr Solosnyuk <asolosnyuk@mail.ru>
2025-04-21 20:19:21 +0300

    api test#1 error ref

--- report_portal/src/test/java/taskApi/reportPortal/ApiTest.java
+++ report_portal/src/test/java/taskApi/reportPortal/ApiTest.java
@@ -7,19 +7,20 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 public class ApiTest {
+    String accessToken = "newtest_ymp_qg_mQ06P70fwG6Wo172jaA4XbYYaaPDqDGv4QwkN_5dHL-xK3R2PWmGYJt9w";
 
     @Test
     public void successPostTest() {
-        String baseUrl = "https://demo.reportportal.io/v1/";
-        String baseApi = "/projectName/dashboard";
-        String accessToken = "newtest_ymp_qg_mQ06P70fwG6Wo172jaA4XbYYaaPDqDGv4QwkN_5dHL-xK3R2PWmGYJt9w";
+        String baseUrl = "https://demo.reportportal.io/api/v1/";
+        String baseApi = "/dashboard";
         String name = "postd";
 
         given()
                 .baseUri(baseUrl + name + baseApi)
+                .header("accept", "application/json")
                 .header("Authorization", "Bearer " + accessToken)
                 .contentType(ContentType.JSON)
-                .body("{\"description\": \"string\", \"name\": \"string\"}")
+                .body("{\"description\": \"string\", \"name\": \"postd\"}")
                 .post()
                 .then()
                 .statusCode(201);
